ee873402523b3ed82251c5a1a7c797cbc1e5f8e3
Product0wner <jaanbrunken@gmail.com>
2020-11-22 20:51:59 +0100

    basic component set up done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import SearchForm from './components/SearchForm'
+import MainNav from './components/MainNav'
+import PhotoList from './components/PhotoList'
+
+class App extends Component {
+  state = {
+    images: "Ballaman"
+  };
+
+  render() {
+    return (
+      <div class="container">
+      <SearchForm/>
+      <MainNav/>
+      <PhotoList/>
     </div>
-  );
+    ) 
+  } 
 }
-
 export default App;
---
+++ src/components/PhotoList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Images from './Images'
+
+const PhotoList = () => {
+    return(
+        <div class="photo-container">
+            <h2>Results</h2>
+            <Images/>
+        </div>
+    )
+}
+
+export default PhotoList;
